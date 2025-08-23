e59bd964ff4c784077b0bb924750b7d621f56635
Ian <iancunha20@gmail.com>
2025-08-22 22:10:18 -0300

    Ajuste Sobre

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -7,7 +7,7 @@ const About: React.FC = () => (
         animate={{ opacity: 1 }}
         transition={{ delay: 0.5, duration: 0.6 }}
     >
-        Como desenvolvedor, busco criar tecnologia com impacto social, um compromisso que me rendeu a indicação como finalista do Prêmio Innovare em 2024. Meu trabalho é pautado pela mentalidade DevOps, focando na entrega de soluções eficientes e de alta qualidade, dedicação que aplico também como voluntário em uma ONG. Hoje, meu objetivo é construir uma carreira sólida que me permita viajar o mundo. Nas horas vagas, gosto de treinar, ouvir música e aprender algo novo.
+        Como desenvolvedor, busco criar tecnologia com impacto social, um compromisso que me rendeu a indicação como finalista do Prêmio Innovare em 2024. Meu trabalho é pautado pela mentalidade DevOps, focando na entrega de soluções eficientes e de alta qualidade, dedicação que aplico também como voluntário em uma ONG. Hoje, meu objetivo é construir uma carreira sólida que me permita viajar o mundo. Nas horas vagas, gosto de treinar, ouvir música ou aprender algo novo.
     </S.About>
 );
 
